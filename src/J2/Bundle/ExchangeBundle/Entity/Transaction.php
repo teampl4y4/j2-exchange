38ac12788cec0138438c54328c4ec30b743507e8
Josh Phipps <josh@studiogood.com>
2012-07-11 13:59:53 -0500

    new transaction entity

---
+++ src/J2/Bundle/ExchangeBundle/Entity/Transaction.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace J2\Bundle\ExchangeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * J2\Bundle\ExchangeBundle\Entity\Transaction
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="J2\Bundle\ExchangeBundle\Entity\TransactionRepository")
+ */
+class Transaction
+{
+    /**
+     * @var integer $id
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var datetime $createdAt
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @var float $amount
+     *
+     * @ORM\Column(name="amount", type="float")
+     */
+    private $amount;
+
+    /**
+     * @var string $description
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param datetime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return datetime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set amount
+     *
+     * @param float $amount
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * Get amount
+     *
+     * @return float 
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
