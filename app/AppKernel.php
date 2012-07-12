30271bf9087edc3d16c22384135a4256c74f5123
Josh Team <joshteam@gmail.com>
2012-07-11 23:39:22 -0500

    moving primary navigation to the KnpMenu bundle with LeftNavigation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
-            new J2ExchangeBundle()
+            new J2ExchangeBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/J2/Bundle/ExchangeBundle/Menu/LeftNavigation.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Left Primary Navigations
+ */
+
+namespace J2\Bundle\ExchangeBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class LeftNavigation extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root', array('childrenAttributes' => array('id' => 'menu')));
+
+        $menu->addChild('Dashboard', array('route' => '_dashboard', 'attributes' => array('class' => 'dash')));
+        $menu->addChild('Products', array('route' => '_products', 'attributes' => array('class' => 'products')));
+        $menu->addChild('Offers', array('route' => '_offers', 'attributes' => array('class' => 'offers')));
+
+        return $menu;
+    }
+}
