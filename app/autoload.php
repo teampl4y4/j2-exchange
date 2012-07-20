82fce8e117ff6256150ec7f5b7b65195182cbfa9
Josh Team <joshteam@gmail.com>
2012-07-20 00:11:08 -0500

    ...

--- app/autoload.php
+++ app/autoload.php
@@ -18,7 +18,7 @@ $loader->registerNamespaces(array(
     'Knp\Bundle'       => __DIR__.'/../vendor/bundles',
     'Knp\Menu'         => __DIR__.'/../vendor/KnpMenu/src',
     'Bazinga'          => __DIR__.'/../vendor/bundles',
-    'Faker'            => __DIR__.'/../vendor/faker/src',
+    'Faker'            => __DIR__.'/../vendor/faker/src'
 ));
 $loader->registerPrefixes(array(
     'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
