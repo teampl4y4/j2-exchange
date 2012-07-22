6d06e2b6a2b3ea0e110990423bb7349c3951e781
Josh Team <joshteam@gmail.com>
2012-07-21 23:49:19 -0500

    working on offers page

---
+++ src/J2/Bundle/ExchangeBundle/Twig/Text.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: jteam
+ * Date: 7/21/12
+ * Time: 11:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Text
+{
+
+}
